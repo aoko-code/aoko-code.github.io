440a1b6b707fe8f3d1dfc75eac4e883b96eee91f
aoko-code <cynthiakhamenge@yahoo.com>
2023-06-03 23:22:39 +0300

    projmenu

--- src/Components/ProjMenu.js
+++ src/Components/ProjMenu.js
@@ -1,23 +1,42 @@
 import React from 'react'
+export const categories=[
+  {
+      id: 1,
+      name: 'All',
+      value: 'all'
+  },
+  {
+      id: 2,
+      name: 'Web Development',
+      value: 'web'
+  },
+  {
+      id: 3,
+      name: 'UI/UX Design',
+      value: 'design'
+  },
+]
 
-function ProjMenu() {
+function ProjMenu( activeCategory, setActiveCategory, categories) {
   return (
     <>
     <div className="row text-center pb-3">
     <h3>Projects</h3>  
-        
     </div>
+        {categories.map((category) => (
     <div className="row align-items-center pb-3" data-aos="fade-up" data-aos-delay="100">
     <div className="col-lg-12 d-flex justify-content-center">
-        <ul id="project-filters">
-            <li data-filter="*" className="filter-active">All</li>
-            <li data-filter=".app">App</li>
-            {/* <li data-filter=".filter-card">Card</li> */}
-            <li data-filter=".web">Web</li>
-        </ul>
+        <button id="project-filters" 
+        key={category.value}
+        className={category.value === activeCategory ? 'active' : ''}
+        onClick={() => setActiveCategory(category.value)}> {category.name}
+          
+        </button>
         
     </div>
+    
 </div>
+))}
 </>
   )
 }
