8504ebdb7ae3c5d48707f98efb763846c7593dc5
aoko-code <cynthiakhamenge@yahoo.com>
2024-02-04 16:19:32 +0300

    Updates

---
+++ precache-manifest.032aad36c948e4394e7aa7c40af32971.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45de3fb7641bfdee46dbbef971697cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3017b61df69bbe153865",
+    "url": "/static/css/main.e54eb815.chunk.css"
+  },
+  {
+    "revision": "8f467b93b755d3f1b7d7",
+    "url": "/static/js/2.aac0de60.chunk.js"
+  },
+  {
+    "revision": "6465fb6b50e3e88618532c78f93b860b",
+    "url": "/static/js/2.aac0de60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b3b38748ee93303e4fa",
+    "url": "/static/js/3.d390f4ff.chunk.js"
+  },
+  {
+    "revision": "3017b61df69bbe153865",
+    "url": "/static/js/main.5ec3a0a2.chunk.js"
+  },
+  {
+    "revision": "1637429d7240950a4978",
+    "url": "/static/js/runtime-main.1045c963.js"
+  },
+  {
+    "revision": "d62c49efd695da085266164efdd4b480",
+    "url": "/static/media/bios.d62c49ef.jpeg"
+  },
+  {
+    "revision": "884636d8e44b5ff352221170eb29ccb0",
+    "url": "/static/media/calc.884636d8.jpeg"
+  },
+  {
+    "revision": "c3beb2a62d940f05d26cd9ca76a3721e",
+    "url": "/static/media/cyber.c3beb2a6.jpg"
+  },
+  {
+    "revision": "1dd081f88a1b9ba7a6fbb7fc7f50e390",
+    "url": "/static/media/cynthia.1dd081f8.jpg"
+  },
+  {
+    "revision": "92fd67b6dae1a3bdf523da98dfbc659b",
+    "url": "/static/media/db.92fd67b6.png"
+  },
+  {
+    "revision": "d7b47937e636c73dcf4cbf012703cd92",
+    "url": "/static/media/lakehub.d7b47937.png"
+  },
+  {
+    "revision": "46b85e0eb534358750f061dc51e2586b",
+    "url": "/static/media/os.46b85e0e.png"
+  },
+  {
+    "revision": "45cacc144fe7ddf22a39cd89a06eed1c",
+    "url": "/static/media/plp.45cacc14.jpeg"
+  },
+  {
+    "revision": "4782b5f3f66dd8251bcf129955c89342",
+    "url": "/static/media/psql.4782b5f3.png"
+  },
+  {
+    "revision": "29db72dc3fbbe1fab5aa9028281ba3ab",
+    "url": "/static/media/py.29db72dc.png"
+  },
+  {
+    "revision": "51f5d6cf2edf00caa257ed1d7a157135",
+    "url": "/static/media/remas.51f5d6cf.jpeg"
+  },
+  {
+    "revision": "3a76933041707b0b4067ba569af1bdfb",
+    "url": "/static/media/sdlc.3a769330.jpg"
+  },
+  {
+    "revision": "f42f51cf0fe35922256f75951187c037",
+    "url": "/static/media/web.f42f51cf.png"
+  },
+  {
+    "revision": "d147688c4e95f043cf8a4392d43fe522",
+    "url": "/static/media/webpack.d147688c.jpg"
+  }
+]);
